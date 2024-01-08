988809334df5189fdbd99fedb116cd94c625b43d
David Bean <heinzbeansss@gmail.com>
2024-01-08 22:30:08 +0000

    Setup simple routing, a collection of posts, dynamic loading of said posts, parsing meta data to use when creating a Post object with the new Post class.

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+
+    <?php foreach ($posts as $post) : ?>
+
+        <article>
+            <h1>
+                <a href="/posts/<?= $post->slug; ?>">
+                    <?= $post->title ?>
+                </a>
+            </h1>
+            
+            <div><?= $post->excerpt; ?></div>
+        </article>
+
+    <?php endforeach; ?>
+</body>
